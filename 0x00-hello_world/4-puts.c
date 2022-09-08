f8b883cc0c3a79005fbb1e610cb5144828620867
Amos Daghamu <adaghamu3@gmail.com>
2022-09-08 06:35:43 -0700

    Adding solution to task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+
 /**
-this file starts at main
-and exits by returning 0;
-**/
+
+ * main - main block
+
+ * Return: 0
+
+ */
+
 int main(void)
+  
 {
+  
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  
+  return (0);
+  
 }

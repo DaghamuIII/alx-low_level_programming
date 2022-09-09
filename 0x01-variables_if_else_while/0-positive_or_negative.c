5a1a51b60cdc10c86ed16df061e13e6d23a6901b
Amos Daghamu <adaghamu3@gmail.com>
2022-09-09 02:55:15 -0700

    Hot fix on sol 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,14 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 #include <time.h>
 
+#include <stdlib.h>
+
 /**
-* main -starting point
-* Description -prints wheather random number is positive or negative
-* Return: 0(success)
+* main - main block
+* Description: Get a random number and print the number
+* and if it is positive, negative, or zero
+* Return: 0
 */
 
 int main(void)
@@ -14,12 +17,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n == 0)
-		printf(0 + " is zero\n");
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
 	else
-		printf("%d is positive\n", n);
+		printf("%i is zero\n", n);
 	return (0);
 }

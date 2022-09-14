f21e9f28de59cdf543b8fccbf2fc02ae0b33a713
Amos Daghamu <adaghamu3@gmail.com>
2022-09-13 18:10:33 -0700

    Adding solution to task 16

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* _isalpha - checks wheather a given character is letter on alphabet or not
+*
+* Description: return 1 if alphabeticl letter and 0 otherwise
+*
+* Return: 1 (alphabetic letter) 0 (other wise)
+*/
+int _isal

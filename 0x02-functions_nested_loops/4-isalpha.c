e733e84fbdc0b5c7ecddf44b0bccdcb0d9aa63d6
Amos Daghamu <adaghamu3@gmail.com>
2022-09-14 06:53:05 -0700

    Adding sol to task 4, hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
 * _isalpha - checks wheather a given character is letter on alphabet or not
-*
+*@c: character of char type
 * Description: return 1 if alphabeticl letter and 0 otherwise
 *
 * Return: 1 (alphabetic letter) 0 (other wise)
 */
 int _isalpha(int c)
 {
-	if ((c > 94 && c < 91) || (c > 96 && c < 123))
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
 		return (1);
 	else
 		return (0);

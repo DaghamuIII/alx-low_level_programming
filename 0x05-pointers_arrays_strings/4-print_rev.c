4a09a09762ab9559f4cc1122ae25efea2f8e6f6e
Amos Daghamu <adaghamu3@gmail.com>
2022-09-19 03:48:19 -0700

    Adding solution to task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,6 +8,7 @@ void print_rev(char *s)
 {
 	/* to find number of cahracter */
 	int i;
+	int j;
 	int str_len;
 
 	for (i = 0; s[i] != '\0'; i++)
@@ -15,8 +16,6 @@ void print_rev(char *s)
 	str_len = i;
 
 	/* printing in reverse */
-
-	int j;
 
 	for (j = (str_len - 1); j >= 0; j--)
 		_putchar(s[j]);

531eee987ebfb60f3d6144b6241ae0ba5a154d2b
Amos Daghamu <adaghamu3@gmail.com>
2022-09-28 05:34:11 -0700

    Hot fix tax 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,62 +1,98 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* wildcmp - check the code for Holberton School students.
-* @s1: string
-* @s2: string
-* Return: Always 0.
+* wildcmp - compares two strings considering the wildcard *
+* @s1: first string to be compared
+* @s2: second string to be compared, can contain the special character *
+*
+* Return: 1 if the strings can be considered identical, 0 otherwise
 */
 int wildcmp(char *s1, char *s2)
 {
-	return (checker(s1, s2, 0, 0, -1));
+	char *last_sc = 0;
+	char *last_s1_sc = 0;
+
+	return (full_compare(s1, s2, last_sc, last_s1_sc));
 }
 /**
-* checkLast - check last char of s2 when s1 ends
-* @s: string
-* @i: int
-* Return: 0 or 1
+* full_compare - auxiliar function to work with recursion
+* @s1: first string to be compared
+* @s2: second string to be compared, can contain the special character *
+* @last_sc: last position in s2 where a char after a * was found
+* @last_s1_sc: position in string s1 where comparison began after found a *
+*
+* Return: 1 if the strings can be considered identical, 0 otherwise
 */
-int checkLast(char *s, int i)
+int full_compare(char *s1, char *s2, char *last_sc, char *last_s1_sc)
 {
-	if (s[i] == '*')
-		return (checkLast(s, i + 1));
-	else if (s[i] == '\0')
+	int sc = 0;
+
+	if (*s2 == '*')
+	{
+		sc = 1;
+		if (!find_next_c(&s2, s2))
+			return (1);
+		last_sc = s2;
+		last_s1_sc = s1;
+	}
+	if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (1);
+		return (1 * full_compare(s1 + 1, s2 + 1, last_sc, last_s1_sc));
+	}
+	if (sc || (*(s2 - 1) == '*'))
+	{
+		if (!find_after_sc(&s1, s1, *s2))
+			return (0);
+		return (1 * full_compare(s1 + 1, s2 + 1, last_sc, last_s1_sc));
+	}
+	if (last_sc && *s2 != '\0')
+	{
+		s2 = last_sc;
+		s1 = last_s1_sc + 1;
+		full_compare(s1, s2, last_sc, last_s1_sc);
 		return (1);
+	}
 	return (0);
 }
 /**
-* checker - helper
-* @s1: string
-* @s2: string
-* @a: int
-* @b: int
-* @wildUsed: int
-* Return: Always 0.
+* find_next_c - finds next character different than * and update s2 address
+* @s2: address of pointer to s2, used to update s2 addrss out of this function
+* @as2: auxiliar pointer to later update s2 addrss
+*
+* Return: 1 if character is found, 0 otherwise.
 */
-int checker(char *s1, char *s2, int a, int b, int wildUsed)
+int find_next_c(char **s2, char *as2)
 {
-	if (s1[a] != '\0')
+	if (*as2 == '\0')
+		return (0);
+	if (*as2 != '*')
 	{
-		if (s2[b] == '\0')
-			return (0);
-		else if (s2[b] == '*')
-		{
-			if (s2[b + 1] == '*')
-				return (checker(s1, s2, a, b + 1, b));
-			else if (s2[b + 1] == s1[a])
-				return (checker(s1, s2, a, b + 1, b));
-			else if (s1[a + 1] != s2[b + 1])
-				return (checker(s1, s2, a + 1, b, b));
-			else if (s1[a + 1] == s2[b + 1])
-				return (checker(s1, s2, a + 1, b + 1, b));
-		}
-		else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
-			return (checker(s1, s2, a + 1, b + 1, wildUsed));
-		if (wildUsed == -1)
-			return (0);
-		return (checker(s1, s2, a, wildUsed, wildUsed));
+		*s2 = as2;
+		return (1);
 	}
-	if (s2[b] != '\0')
-		return (checkLast(s2, b));
+	if (find_next_c(s2, as2 + 1) == 0)
+		return (0);
+	return (1);
+}
+/**
+* find_after_sc - finds the next character in s1 mathing in s2 address
+* @s1: address of pointer to s1, used to update s1 addrss out of this function
+* @as1: auxiliar pointer to later update s1 addrss
+* @s2: character to match within s1
+*
+* Return: 1 if character is found, 0 otherwise.
+*/
+int find_after_sc(char **s1, char *as1, char s2)
+{
+	if (*as1 == '\0')
+		return (0);
+	if (*as1 == s2)
+	{
+		*s1 = as1;
+		return (1);
+	}
+	if (find_after_sc(s1, as1 + 1, s2) == 0)
+		return (0);
 	return (1);
 }

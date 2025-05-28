0f43ecec116593eba4199e7b7ea8be7d98cdbfcc
Amos Nyirenda <85877874+DaghamuIII@users.noreply.github.com>
2025-05-28 22:13:19 +0200

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,10 +16,25 @@ int _strlen(const char *s)
 }
 
 /**
+ * _strdup - duplicate a string to new memory
+ * @s: string to duplicate
+ * Return: pointer to duplicated string or NULL
+ */
+char *_strdup(const char *s)
+{
+	int len = _strlen(s);
+	char *copy = malloc(len + 1);
+	if (!copy)
+		return (NULL);
+	for (int i = 0; i <= len; i++)
+		copy[i] = s[i];
+	return (copy);
+}
+
+/**
  * add_node_end - adds a new node at the end of a list_t list
  * @head: double pointer to the head
  * @str: string to duplicate into the node
- *
  * Return: address of new element, or NULL on failure
  */
 list_t *add_node_end(list_t **head, const char *str)
@@ -30,19 +45,19 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (head == NULL || str == NULL)
 		return (NULL);
 
-	dupstr = strdup(str);  // assume strdup is allowed
+	dupstr = _strdup(str);
 	if (dupstr == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
-		free(dupstr);  // ❗ Clean up strdup before returning
+		free(dupstr);
 		return (NULL);
 	}
 
 	new->str = dupstr;
-	new->len = _strlen(dupstr);
+	new->len = _strlen(str);
 	new->next = NULL;
 
 	if (*head == NULL)

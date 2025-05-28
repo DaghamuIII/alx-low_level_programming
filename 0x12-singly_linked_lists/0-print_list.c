63f78bd0ecc3c260587559122acb670141bf7489
Amos Nyirenda <85877874+DaghamuIII@users.noreply.github.com>
2025-05-28 17:55:05 +0200

    Update 0-print_list.c betty hotfix 2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
-* print_list - print all elements of `list_t` list
-* @h: head of linked list
-* Return: number of nodes
-*/
-
+ * print_list - print all elements of a list_t list
+ * @h: head of linked list
+ *
+ * Return: number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	const list_t *current;
@@ -17,12 +17,11 @@ size_t print_list(const list_t *h)
 	while (current != NULL)
 	{
 		if (current->str == NULL)
-            		printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", current->len, current->str);
+			printf("[%u] %s\n", current->len, current->str);
 		current = current->next;
 		c++;
 	}
-
 	return (c);
 }

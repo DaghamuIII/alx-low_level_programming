ac99655386db6300519532343258de9539a26336
Amos Nyirenda <85877874+DaghamuIII@users.noreply.github.com>
2025-05-31 05:41:58 +0200

    after 3 years Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,41 @@
 #include "lists.h"
-/**
-* free_listint_safe - free a `listint_t` list and set the head to null
-* @h: double pointer to head of linked list
-* Description: This function should work for circular lists
-* Only loop through the list once
-* Return: size of the list that was free'd
-*/
+#include <stdlib.h>
 
+/**
+ * free_listint_safe - Frees a listint_t list (even if there's a loop)
+ * @h: Pointer to the address of the head
+ * Return: Number of nodes freed
+ */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
+	listint_t *current = *h;
+	listint_t **visited;
+	size_t i, count = 0;
 
-	count = 0;
-	current = *h;
-	while (current != NULL)
+	if (!h || !*h)
+		return (0);
+
+	visited = malloc(sizeof(listint_t *) * 1024); // Max 1024 nodes for now
+	if (!visited)
+		exit(98);
+
+	while (current)
 	{
-		count++;
-		hold = current;
+		for (i = 0; i < count; i++)
+		{
+			if (current == visited[i])
+			{
+				free(visited);
+				*h = NULL;
+				return (count);
+			}
+		}
+		visited[count++] = current;
+		listint_t *temp = current;
 		current = current->next;
-		free(hold);
-
-		if (hold < current)
-			break;
+		free(temp);
 	}
+	free(visited);
 	*h = NULL;
-
 	return (count);
 }
